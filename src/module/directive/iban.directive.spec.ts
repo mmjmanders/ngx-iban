3b0e59d951052756f035da8ce6fb4184a1796874
Mark Manders <mmjmanders@gmail.com>
2017-11-28 15:27:49 +0100

    Added tests

---
+++ src/module/directive/iban.directive.spec.ts
@@ -0,0 +1,90 @@
+import { IbanDirective } from '../iban.module';
+import { FormsModule } from '@angular/forms';
+import { Component, Optional } from '@angular/core';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By, BrowserModule } from '@angular/platform-browser';
+
+@Component({
+    template: `<input type="text" [ngxIban]="iban" [(ngModel)]="iban">`,
+    providers: [IbanDirective]
+})
+export class OptionalIban {
+    public iban: string;
+}
+
+@Component({
+    template: `<input type="text" [ngxIban]="iban" [(ngModel)]="iban" required>`,
+    providers: [IbanDirective]
+})
+export class RequiredIban {
+    public iban: string;
+}
+
+describe('Iban: directive', () => {
+    let optional: ComponentFixture<OptionalIban>;
+    let required: ComponentFixture<RequiredIban>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule,
+                BrowserModule
+            ],
+            declarations: [OptionalIban, RequiredIban, IbanDirective]
+        }).compileComponents();
+
+        optional = TestBed.createComponent(OptionalIban);
+        optional.autoDetectChanges();
+        required = TestBed.createComponent(RequiredIban);
+        required.autoDetectChanges();
+    }));
+
+    it('optional should pass for empty iban', () => {
+        const elem: DebugElement = optional.debugElement;
+        const input: HTMLInputElement = elem.query(By.css('input')).nativeElement as HTMLInputElement;
+        input.value = '';
+        input.dispatchEvent(new Event('input'));
+        expect(input.classList.contains('ng-valid')).toBe(true);
+    });
+
+    it('optional should pass for valid iban', () => {
+        const elem: DebugElement = optional.debugElement;
+        const input: HTMLInputElement = elem.query(By.css('input')).nativeElement as HTMLInputElement;
+        input.value = 'NL64SNSB0930451912';
+        input.dispatchEvent(new Event('input'));
+        expect(input.classList.contains('ng-valid')).toBe(true);
+    });
+
+    it('optional should fail for invalid iban', () => {
+        const elem: DebugElement = optional.debugElement;
+        const input: HTMLInputElement = elem.query(By.css('input')).nativeElement as HTMLInputElement;
+        input.value = 'NL64SNSB0930451911';
+        input.dispatchEvent(new Event('input'));
+        expect(input.classList.contains('ng-valid')).toBe(false);
+    });
+
+    it('required should fail for empty iban', () => {
+        const elem: DebugElement = required.debugElement;
+        const input: HTMLInputElement = elem.query(By.css('input')).nativeElement as HTMLInputElement;
+        input.value = '';
+        input.dispatchEvent(new Event('input'));
+        expect(input.classList.contains('ng-valid')).toBe(false);
+    });
+
+    it('required should pass for valid iban', () => {
+        const elem: DebugElement = required.debugElement;
+        const input: HTMLInputElement = elem.query(By.css('input')).nativeElement as HTMLInputElement;
+        input.value = 'NL64SNSB0930451912';
+        input.dispatchEvent(new Event('input'));
+        expect(input.classList.contains('ng-valid')).toBe(true);
+    });
+
+    it('required should fail for invalid iban', () => {
+        const elem: DebugElement = required.debugElement;
+        const input: HTMLInputElement = elem.query(By.css('input')).nativeElement as HTMLInputElement;
+        input.value = 'NL64SNSB0930451911';
+        input.dispatchEvent(new Event('input'));
+        expect(input.classList.contains('ng-valid')).toBe(false);
+    });
+});
