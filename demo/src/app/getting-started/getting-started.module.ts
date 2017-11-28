39c95125415e3f1d672474fc412c7ab5b2d17c7d
Mark Manders <mmjmanders@gmail.com>
2017-11-28 21:50:07 +0100

    Added style to demo page

--- demo/src/app/getting-started/getting-started.module.ts
+++ demo/src/app/getting-started/getting-started.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GettingStartedComponent } from './getting-started.component';
 import { GettingStartedRoutingModule } from './getting-started-routing.module';
+import { HighlightModule } from 'ngx-highlightjs';
 
 @NgModule({
   imports: [
     CommonModule,
-    GettingStartedRoutingModule
+    GettingStartedRoutingModule,
+    HighlightModule.forRoot()
   ],
   declarations: [GettingStartedComponent],
 })
