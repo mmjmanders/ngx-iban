1d5f49c77e611ea85836e14c530a2649adccbd9a
Mark Manders <mmjmanders@gmail.com>
2017-11-29 06:53:10 +0100

    Added local dependency in demo page

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IbanModule } from 'ngx-iban';
+import { IbanModule } from '../../../../src/index';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
