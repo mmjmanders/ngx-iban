7c5b0c0e76ee66374b5c7cb5f479fd903bd281d3
Mark Manders <mmjmanders@gmail.com>
2021-03-29 14:26:42 +0200

    Moved from iban.js to ibantools #9

--- projects/ngx-iban/src/lib/iban.pipe.ts
+++ projects/ngx-iban/src/lib/iban.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import * as IBAN from "iban";
+import { friendlyFormatIBAN } from "ibantools";
 
 @Pipe({
   name: "iban"
 })
 export class IbanPipe implements PipeTransform {
   transform(value: string, separator?: string): string {
-    return value ? IBAN.printFormat(value, separator) : null;
+    return value ? friendlyFormatIBAN(value, separator) : null;
   }
 }
