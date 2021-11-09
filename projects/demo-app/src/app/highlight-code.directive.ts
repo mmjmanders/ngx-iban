c7d6e2e9a4a9271245c20866a236f3c4f372dcac
Mark Manders <mmjmanders@gmail.com>
2021-11-09 11:43:22 +0100

    Updated highlight.js

--- projects/demo-app/src/app/highlight-code.directive.ts
+++ projects/demo-app/src/app/highlight-code.directive.ts
@@ -1,13 +1,22 @@
-import { AfterViewInit, Directive, ElementRef } from "@angular/core";
-import * as hljs from "highlight.js";
+import { AfterViewInit, Directive, ElementRef, OnInit } from "@angular/core";
+import hljs from "highlight.js/lib/core";
+import bash from "highlight.js/lib/languages/bash";
+import xml from "highlight.js/lib/languages/xml";
+import typescript from "highlight.js/lib/languages/typescript";
 
 @Directive({
   selector: "code[highlight]"
 })
-export class HighlightCodeDirective implements AfterViewInit {
+export class HighlightCodeDirective implements OnInit, AfterViewInit {
   constructor(private el: ElementRef) {}
 
+  ngOnInit() {
+    hljs.registerLanguage("bash", bash);
+    hljs.registerLanguage("html", xml);
+    hljs.registerLanguage("typescript", typescript);
+  }
+
   ngAfterViewInit() {
-    hljs.highlightBlock(this.el.nativeElement);
+    hljs.highlightElement(this.el.nativeElement);
   }
 }
