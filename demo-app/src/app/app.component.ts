e280144b78e19625f424a7ca73b34939fb9d940e
Mark Manders <mmjmanders@gmail.com>
2023-11-13 13:59:16 +0100

    Revert "Reverted to modular approach"
    This reverts commit 55b939c1340c6ed15409355d7618915cf54d3862.

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faCreditCard,
+  faFileLines,
+  faPersonChalkboard,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule, FontAwesomeModule],
   selector: 'ngx-iban-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(readonly library: FaIconLibrary) {
+    library.addIcons(faCreditCard, faFileLines, faPersonChalkboard);
+  }
+}
