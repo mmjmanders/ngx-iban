987060be78e73d32855741a96e4597201cb8890f
Mark Manders <mmjmanders@gmail.com>
2026-01-30 10:11:19 +0100

    Migrate ESLint configuration to flat config format and update dependencies to support new structure.

---
+++ eslint.config.mjs
@@ -0,0 +1,71 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nx from '@nx/eslint-plugin';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(['**/*', '**/node_modules']),
+  {
+    plugins: {
+      '@nx': nx,
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: compat.extends('plugin:@nx/typescript'),
+
+    rules: {
+      'no-extra-semi': 'error',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    extends: compat.extends('plugin:@nx/javascript'),
+
+    rules: {
+      'no-extra-semi': 'error',
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+
+    rules: {},
+  },
+]);
